e87251cb78f8672f57313ab48200098fc54ebab3
Julio Felipe <julio@informach.com.br>
2022-06-17 18:13:49 -0300

    feat: dashboard finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import {Routes as Switch, Route} from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import CourseDetail from './CourseDetail';
@@ -6,8 +7,12 @@ import Footer from './Footer';
 import Login from './User/Login';
 import Register from './User/Register';
 import Dashboard from './User/Dashboard';
+import MyCourses from './User/MyCourses';
+import FavoriteCourses from './User/FavoriteCourses';
+import RecommendedCourses from './User/RecommendedCourses';
+import ProfileSetting from './User/ProfileSetting';
+import ChangePassword from './User/ChangePassword';
 
-import {Routes as Switch, Route} from 'react-router-dom';
 
 function Main() {
   return (
@@ -20,6 +25,11 @@ function Main() {
         <Route path="/user-login" element={<Login />} />
         <Route path="/user-register" element={<Register />} />
         <Route path="/user-dashboard" element={<Dashboard />} />
+        <Route path="/my-courses" element={<MyCourses />} />
+        <Route path="/favorite-courses" element={<FavoriteCourses />} />
+        <Route path="/recommended-courses" element={<RecommendedCourses />} />
+        <Route path="/profile-setting" element={<ProfileSetting />} />
+        <Route path="/change-password" element={<ChangePassword />} />
       </Switch>
       <Footer />
     </div>
--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -6,21 +6,21 @@ function Login() {
                     <div className="card">
                         <h5 className="card-header">User Login</h5>
                         <div className="card-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" className="form-label">UserName</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            </div>
-                            <div className="mb-3">
-                                <label for="exampleInputPassword1" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" />
-                            </div>
-                            <div className="mb-3 form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                <label className="form-check-label" for="exampleCheck1">Remember Me</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </form>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">UserName</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                                </div>
+                                <div className="mb-3 form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
